6e7bd1c6ffc198ca0a73d96fa24c2b26951b9220
otutay <otutaysalgir@gmail.com>
2019-12-28 23:59:02 +0300

    ram added

--- srcRtl/design/ram.vhd
+++ srcRtl/design/ram.vhd
@@ -6,7 +6,7 @@
 -- Author     : osmant  <otutaysalgir@gmail.com>
 -- Company    :
 -- Created    : 2019-12-22
--- Last update: 2019-12-22
+-- Last update: 2019-12-28
 -- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -28,28 +28,69 @@ entity ram is
 
   generic (
     cRamPerformance : tPerfEnum := highPerf;  -- highPerf, lowLatency
-    cRamInitFile    : string                  -- leave blank if not used
+    cRamInitFile    : string                  -- leave blank if not used -- not
+                                              -- supported
     );
 
   port (
-    iClk : in std_logic;                -- clk
-    iRst : in std_logic               -- rst
+    iClk   : in std_logic;              -- clk
+    iRst   : in std_logic;              -- rst
+    iRamIn : in tRamData
     );
 end entity ram;
 
 architecture rtl of ram is
+  alias clk         : std_logic is iClk;
   signal addr       : std_logic_vector(log2(cRamDepth)-1 downto 0) := (others => '0');
   signal dataIn     : std_logic_vector(cRamWidth-1 downto 0)       := (others => '0');
+  signal ramBlock   : tRamArray                                    := (others => (others => '0'));
+  signal ramData    : std_logic_vector(cRamWidth-1 downto 0)       := (others => '0');
   signal wEn        : std_logic                                    := '0';
   signal en         : std_logic                                    := '0';
-  signal rst        : std_logic                                    := '0';
-  signal outRegEn   : std_logic                                    := '0';
-  signal dataOutReg : std_logic_vector(cRamWidth-1 downto 0) : =(others       => '0');
-  signal dataOut    : std_logic_vector(cRamWidth-1 downto 0) : =(others       => '0');
+  signal dataOutReg : std_logic_vector(cRamWidth-1 downto 0);  -- : =(others       => '0');
+  signal dataOut    : std_logic_vector(cRamWidth-1 downto 0);  -- : =(others       => '0');
 
 
 begin  -- architecture rtl
+  inputRegPro : process (clk) is
+  begin  -- process inputRegPro
+    if clk'event and clk = '1' then     -- rising clock edge
+      dataIn <= iRamIn.data;
+      addr   <= iRamIn.addr;
+      en     <= iRamIn.en;
+      wEn    <= iRamIn.wEn;
+    end if;
+  end process inputRegPro;
 
+  ramReadWritePro : process (clk) is
+  begin  -- process ramReadWritePro
+    if clk'event and clk = '1' then     -- rising clock edge
+      if(en = '1') then
+        if (wEn = '1') then
+          ramBlock(to_integer(unsigned(addr))) <= dataIn;
+        else
+          ramData <= ramBlock(to_integer(unsigned(addr)));
+        end if;
+      end if;
+    end if;
+  end process ramReadWritePro;
 
+  lowLatencyGen : if cRamPerformance = lowLatency generate
+    dataOut <= ramData;
+  end generate lowLatencyGen;
+
+  highPerfGen : if cRamPerformance = highPerf generate
+    OutDataPro : process (clk) is
+    begin  -- process OutDataPro,
+      if clk'event and clk = '1' then   -- rising clock edge
+        if(iRst = '1') then
+          dataOutReg <= (others => '0');
+        else
+          dataOutReg <= ramData;
+        end if;
+      end if;
+    end process OutDataPro;
+    dataOut <= dataOutReg;
+  end generate highPerfGen;
 
 end architecture rtl;
--- srcRtl/design/ramPckg.vhd
+++ srcRtl/design/ramPckg.vhd
@@ -6,7 +6,7 @@
 -- Author     : osmant  <otutaysalgir@gmail.com>
 -- Company    :
 -- Created    : 2019-12-22
--- Last update: 2019-12-22
+-- Last update: 2019-12-28
 -- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -21,25 +21,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+USE std.textio.all;
 
 package ramPckg is
-  constant cRamWidth : integer := 8;         -- dataBitW
-  constant cRamDepth : integer := 16;        -- ram depth
-  type tPerfEnum is (highPerf, lowLatency);  -- ram performance enum
+  constant cRamWidth            : integer := 8;   -- dataBitW
+  constant cRamDepth            : integer := 16;  -- ram depth
+  type tPerfEnum is (highPerf, lowLatency);       -- ram performance enum
+  function log2 (constant depth : integer) return integer;
+
 
   type tRamArray is array (cRamDepth-1 downto 0) of std_logic_vector(cRamWidth-1 downto 0);
 
-  function log2 (
-  constant depth : integer)
-  return integer;
+  type tRamData is record
+    addr : std_logic_vector(log2(cRamDepth)-1 downto 0);
+    data : std_logic_vector(cRamWidth-1 downto 0);
+    wEn  : std_logic;
+    en   : std_logic;
+  end record tRamData;
+  constant cRamData : tRamData := ((others => '0'), (others => '0'), '0', '0');
+
 
 end package ramPckg;
 
 package body ramPckg is
 
-    function log2 (
-    constant depth : integer)
-    return integer is
+ function log2 (constant depth : integer) return integer is
     variable temp   : integer := depth;
     variable retVal : integer := 0;
   begin  -- function log2
